0ddec30a0d832bd3328b59a626f3c08718065efd
Techatach <techatach@gmail.com>
2025-01-26 13:53:58 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Landingpage from "@/components/Landingpage";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
-      <Button>Button</Button>
+      {/* <Button>Button</Button> */}
       <Landingpage />
     </div>
   );
